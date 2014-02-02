24a23a9c473e79b771bfb01d8ae2d1ef33084dbb
Jan Hancic <jan.hancic@gmail.com>
2014-02-02 02:15:23 +0000

    replace typeof check with 'in' check

--- fasterForEach.js
+++ fasterForEach.js
@@ -9,7 +9,7 @@ Array.prototype.forEach = function(itemProcessor, context) {
 
 	loopHeader = 'var item;\n' +
 				'for (var $__i = 0, $__len = this.length; $__i < $__len; $__i++) {\n' +
-					'\tif (typeof this[$__i] === "undefined") { continue; }\n' +
+					'\tif (!($__i in this)) { continue; }\n'
 					'\titem = this[$__i];\n' +
 					'\tidx = $__i;\n' +
 					'\tarray = this;\n';
