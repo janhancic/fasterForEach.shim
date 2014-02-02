bd0aa61c8db4d9c3d7eee343604fad7bb26b7d8d
Jan Hancic <jan.hancic@gmail.com>
2014-02-02 19:46:40 +0000

    Some cleanup

--- fasterForEach.js
+++ fasterForEach.js
@@ -1,3 +1,6 @@
+// ;if (...) {
+Array.prototype.forEach.__itemProcessorMatcher = '';
+
 Array.prototype.forEach = function(itemProcessor, context) {
 	var itemProcessorSource = itemProcessor.toString(),
 		loopHeader;
@@ -5,18 +8,18 @@ Array.prototype.forEach = function(itemProcessor, context) {
 	// TODO:
 	// - parse out the itemProcessor argument names, right now it's all hardcoded
 	// - add support for the context argument
-	// only add forEach if it isn't already present (duh)
+	// - only add forEach if it isn't already present (duh)
 
 	loopHeader = 'var item, idx, array;\n' +
-				// 'var $__i = 0, $__len = this.length;\n' +
-				'for (var $__i = 0, $__len = $__array.length; $__i < $__len; $__i++) {\n' +
-				// 'while (++$__i < $__len) {\n' +
-					'\tif (!($__i in $__array)) { continue; }\n' +
-					'\titem = $__array[$__i];\n' +
-					'\tidx = $__i;\n' +
+				// 'var __i__ = 0, __len__ = this.length;\n' +
+				'for (var __i__ = 0, __len__ = $__array.length; __i__ < __len__; __i__++) {\n' +
+				// 'while (++__i__ < __len__) {\n' +
+					'\tif (!(__i__ in $__array)) { continue; }\n' +
+					'\titem = $__array[__i__];\n' +
+					'\tidx = __i__;\n' +
 					'\tarray = $__array;\n';
 
-	itemProcessorSource = itemProcessorSource.replace('function (item, idx, array) {', loopHeader);
+	itemProcessorSource = itemProcessorSource.replace('function(item, idx, array) {', loopHeader);
 
 	// console.log(itemProcessorSource);
 	// console.log(context);
@@ -24,3 +27,4 @@ Array.prototype.forEach = function(itemProcessor, context) {
 	// eval(itemProcessorSource);
 	(new Function('$__array', itemProcessorSource))(this);
 };
+// }
