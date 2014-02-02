2f1f6f9ec96ec49bc5dafd21fe918e9722b33c0a
Jan Hancic <jan.hancic@gmail.com>
2014-02-02 20:40:16 +0000

    optimisations

--- fasterForEach.js
+++ fasterForEach.js
@@ -8,30 +8,28 @@ Array.prototype.forEach = function(itemProcessor, context) {
 		argumentNames;
 
 	argumentNames = itemProcessorSource.slice(itemProcessorSource.indexOf('(') + 1, itemProcessorSource.indexOf(')'));
-	argumentNames = argumentNames.match(Array.prototype.forEach.__fasterForEachItemProcessorMatcher__);
+	argumentNames = argumentNames.match(Array.prototype.forEach.__fasterForEachItemProcessorMatcher__) || [];
 
 	// TODO:
 	// - only add forEach if it isn't already present (duh)
 
 	if (argumentNames[0]) {
-		loopHeader = 'var ' + argumentNames[0] + ';\n';
-		innerLoop = '\t' + argumentNames[0] + ' = __array__[__i__];\n';
+		loopHeader = 'var ' + argumentNames[0];
+		innerLoop = argumentNames[0] + ' = __array__[__i__]; ';
 	}
 
 	if (argumentNames[1]) {
-		loopHeader += 'var ' + argumentNames[1] + ';\n';
-		innerLoop += '\t' + argumentNames[1] + ' = __i__;\n';
+		loopHeader = loopHeader.concat(', ' + argumentNames[1]);
+		innerLoop = innerLoop.concat(argumentNames[1] + ' = __i__; ');
 	}
 
 	if (argumentNames[2]) {
-		loopHeader += 'var ' + argumentNames[2] + ' = __array__;\n';
+		loopHeader = loopHeader.concat(', ' + argumentNames[2] + ' = __array__');
 	}
 
-	loopHeader += 'for ( ; __i__ < __len__; __i__++) {\n' +
-					'\tif (!(__i__ in __array__)) { continue; }\n' +
-					innerLoop;
+	loopHeader = loopHeader.concat('; for ( ; __i__ < __len__; __i__++) { if (!(__i__ in __array__)) { continue; }', innerLoop);
 
-	itemProcessorSource = loopHeader + '\n' + itemProcessorSource.substr(itemProcessorSource.indexOf('{') + 1);
+	itemProcessorSource = loopHeader + itemProcessorSource.substr(itemProcessorSource.indexOf('{') + 1);
 
 	(new Function('__i__', '__len__', '__array__', itemProcessorSource)).call(context, 0, this.length, this);
 };
