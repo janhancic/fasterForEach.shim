ec62137688f095df46513b93f4b95753c93282ca
Jan Hancic <jan.hancic@gmail.com>
2014-02-02 01:59:52 +0000

    first WIP version

--- fasterForEach.js
+++ fasterForEach.js
@@ -1,4 +1,18 @@
 Array.prototype.forEach = function(itemProcessor, context) {
-	console.log(itemProcessor);
+	var itemProcessorSource = itemProcessor.toString(),
+		loopHeader;
+
+	loopHeader = 'var item;\n' +
+				'for (var \$__i = 0, \$__len = this.length; \$__i < \$__len; \$__i++) {\n' +
+					'\tif (typeof this[\$__i] === "undefined") { continue; }\n' +
+					'\titem = this[\$__i];\n' +
+					'\tidx = $__i;\n' +
+					'\tarray = this;\n';
+
+	itemProcessorSource = itemProcessorSource.replace('function (item, idx, array) {', loopHeader);
+
+	console.log(itemProcessorSource);
 	console.log(context);
+
+	eval(itemProcessorSource);
 };
