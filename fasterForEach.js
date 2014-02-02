315999ef0e2cd5f829d42016b6d34f851bd00e4b
Jan Hancic <jan.hancic@gmail.com>
2014-02-02 19:50:46 +0000

    inject __i__ and __len__ as arguments

--- fasterForEach.js
+++ fasterForEach.js
@@ -11,20 +11,18 @@ Array.prototype.forEach = function(itemProcessor, context) {
 	// - only add forEach if it isn't already present (duh)
 
 	loopHeader = 'var item, idx, array;\n' +
-				// 'var __i__ = 0, __len__ = this.length;\n' +
-				'for (var __i__ = 0, __len__ = $__array.length; __i__ < __len__; __i__++) {\n' +
-				// 'while (++__i__ < __len__) {\n' +
-					'\tif (!(__i__ in $__array)) { continue; }\n' +
-					'\titem = $__array[__i__];\n' +
+				'for ( ; __i__ < __len__; __i__++) {\n' +
+					'\tif (!(__i__ in __array__)) { continue; }\n' +
+					'\titem = __array__[__i__];\n' +
 					'\tidx = __i__;\n' +
-					'\tarray = $__array;\n';
+					'\tarray = __array__;\n';
 
-	itemProcessorSource = itemProcessorSource.replace('function(item, idx, array) {', loopHeader);
+	itemProcessorSource = itemProcessorSource.replace('function (item, idx, array) {', loopHeader);
 
 	// console.log(itemProcessorSource);
 	// console.log(context);
 
 	// eval(itemProcessorSource);
-	(new Function('$__array', itemProcessorSource))(this);
+	(new Function('__i__', '__len__', '__array__', itemProcessorSource))(0, this.length, this);
 };
 // }
