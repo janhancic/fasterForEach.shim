d9f830d17fc61ce6eddaec750652753b1f414e9c
Jan Hancic <jan.hancic@gmail.com>
2014-02-15 13:18:02 +0000

    Merge two versions into same file

--- fasterForEach.js
+++ fasterForEach.js
@@ -1,5 +1,4 @@
-// ;if (...) {
-Array.prototype.forEach = function(itemProcessor, context) {
+Array.prototype.fasterForEach = function(itemProcessor, context) {
 	context = context || null;
 
 	var itemProcessorSource = itemProcessor.toString(),
@@ -8,10 +7,7 @@ Array.prototype.forEach = function(itemProcessor, context) {
 		argumentNames;
 
 	argumentNames = itemProcessorSource.slice(itemProcessorSource.indexOf('(') + 1, itemProcessorSource.indexOf(')'));
-	argumentNames = argumentNames.match(Array.prototype.forEach.__fasterForEachItemProcessorMatcher__) || [];
-
-	// TODO:
-	// - only add forEach if it isn't already present (duh)
+	argumentNames = argumentNames.match(Array.prototype.fasterForEach.__fasterForEachItemProcessorMatcher__) || [];
 
 	if (argumentNames[0]) {
 		loopHeader = 'var ' + argumentNames[0];
@@ -27,12 +23,46 @@ Array.prototype.forEach = function(itemProcessor, context) {
 		loopHeader += ', ' + argumentNames[2] + ' = __array__';
 	}
 
-	loopHeader += '; for ( ; __i__ < __len__; __i__++) { if (!(__i__ in __array__)) { continue; }' + innerLoop;
+	loopHeader += '; for ( ; __i__ < __len__; ++__i__) { if (!(__i__ in __array__)) { continue; }' + innerLoop;
 
 	itemProcessorSource = loopHeader + itemProcessorSource.substr(itemProcessorSource.indexOf('{') + 1);
 
 	(new Function('__i__', '__len__', '__array__', itemProcessorSource)).call(context, 0, this.length, this);
 };
 
-Array.prototype.forEach.__fasterForEachItemProcessorMatcher__ = new RegExp('([^\s,]+)', 'g');
-// }
+Array.prototype.fasterForEach.__fasterForEachItemProcessorMatcher__ = new RegExp('([^\s,]+)', 'g');
+
+Array.prototype.fasterForEachEval = function(itemProcessor, context) {
+	context = context || null;
+
+	var itemProcessorSource = itemProcessor.toString(),
+		loopHeader = '',
+		innerLoop = '',
+		argumentNames;
+
+	argumentNames = itemProcessorSource.slice(itemProcessorSource.indexOf('(') + 1, itemProcessorSource.indexOf(')'));
+	argumentNames = argumentNames.match(Array.prototype.fasterForEachEval.__fasterForEachItemProcessorMatcher__) || [];
+
+	if (argumentNames[0]) {
+		loopHeader = 'var ' + argumentNames[0];
+		innerLoop = argumentNames[0] + ' = __array__[__i__]; ';
+	}
+
+	if (argumentNames[1]) {
+		loopHeader += ', ' + argumentNames[1];
+		innerLoop += argumentNames[1] + ' = __i__; ';
+	}
+
+	if (argumentNames[2]) {
+		loopHeader += ', ' + argumentNames[2] + ' = __array__';
+	}
+
+	loopHeader += '; for ( ; __i__ < __len__; ++__i__) { if (!(__i__ in __array__)) { continue; }' + innerLoop;
+
+	itemProcessorSource = loopHeader + itemProcessorSource.substr(itemProcessorSource.indexOf('{') + 1);
+
+	eval('var tmpFnc = function (__i__, __len__, __array__) {' + itemProcessorSource + '};');
+	tmpFnc.call(context, 0, this.length, this);
+};
+
+Array.prototype.fasterForEachEval.__fasterForEachItemProcessorMatcher__ = new RegExp('([^\s,]+)', 'g');
